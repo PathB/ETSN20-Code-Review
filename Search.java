e7f4c3a886eae9cd6c636fc10ae31f07e79f55c1
Patric Balan <pa6101ba-s@student.lu.se>
2021-12-01 17:21:39 +0100

    Done

---
+++ Search.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class Search {
+
+    public static void main(String[] args) {
+            Search s = new Search();
+            s.searchMethod(args[0], args[1]);
+        
+    }
+
+    public void searchMethod(String pattern, String file){
+        try{
+            Scanner scan = new Scanner(new File(file));
+            while(scan.hasNextLine()){
+                String temp= scan.nextLine();
+                if(temp.contains(pattern)){
+                    System.out.println(temp);
+                }
+            }
+
+        } catch (FileNotFoundException e){
+            System.out.println("Invalid file name");
+        }
+    }
+}
